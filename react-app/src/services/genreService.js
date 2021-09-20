a5e3c734b095098dbac4ffa898d1b5fdca41f868
merciof <merciofilho@gmail.com>
2021-09-20 15:15:10 -0300

    add genreService

--- react-app/src/services/genreService.js
+++ react-app/src/services/genreService.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const API_URL = 'http://localhost:8080/api/v1/';
 
 const genreService = {
-    getAllGenres: () => axios.get(API_URL + 'genres.json')
+    getAllGenres: () => axios.get(API_URL + 'genres.json'),
+    addGenre: ( genre ) => axios.post(`${API_URL}genres.json`, genre),
 }
 
 export default genreService;
