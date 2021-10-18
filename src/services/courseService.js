d0d86796b9daf7ddf1bef9a9e53ca089cd1fbc80
Mércio Andrade Filho <merciofilho@gmail.com>
2021-10-18 20:42:22 -0300

    add courseService login method

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -2,7 +2,12 @@ import axios from "axios";
 import API_URL from "../config/environment";
 
 const courseService = {
-  getAllCourses: () => axios.get(API_URL + "courses.json")
+  getAllCourses: () => axios.get(API_URL + "courses.json"),
+  login: () =>
+    axios.post(
+      "https://sebo-rural-rest-api-cakephp.herokuapp.com/api/v1/users/login.json",
+      { email: "merciofilho@gmail.com", password: "123" }
+    )
 };
 
 export default courseService;
