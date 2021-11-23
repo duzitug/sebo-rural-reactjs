edd78a1e34b9490117f586b04b2af76d449b724f
Mércio Andrade Filho <merciofilho@gmail.com>
2021-11-23 10:39:15 -0300

    add Authorization header in bookService

--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -7,8 +7,10 @@ const RESPONSE_TYPE = ".json";
 
 const bookService = {
   getAllBooks: () => axios.get(API_URL + "books" + RESPONSE_TYPE),
-  getBookById: (id: number, token) =>
-    axios.get(API_URL + `books/${id}` + RESPONSE_TYPE),
+  getBookById: (id: number, token: any) =>
+    axios.get(API_URL + `books/${id}` + RESPONSE_TYPE, {
+      headers: { Authorization: `Bearer ${token}` }
+    }),
   addBook: (book: Book) => axios.post(API_URL + "books" + RESPONSE_TYPE, book),
   updateBook: "",
   deleteBook: ""
