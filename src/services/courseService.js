719862660107dd0c091aa9d0f8c298be1ff7965e
Mércio Andrade Filho <merciofilho@gmail.com>
2021-11-23 16:05:19 -0300

    remove login method from courseService

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -5,12 +5,7 @@ const courseService = {
   getAllCourses: (token) =>
     axios.get(API_URL + "courses.json", {
       headers: { Authorization: `Bearer ${token}` }
-    }),
-  login: () =>
-    axios.post(
-      "https://sebo-rural-rest-api-cakephp.herokuapp.com/api/v1/users/login.json",
-      { email: "merciofilho@gmail.com", password: "123" }
-    )
+    })
 };
 
 export default courseService;
