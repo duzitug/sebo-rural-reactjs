b6fd3489add897ce1cf6384ecdcf19ef1beb30a3
Mércio Andrade Filho <merciofilho@gmail.com>
2021-11-26 18:27:48 -0300

    add route for BookListByCourseContainer

--- src/components/book/BookListByCourseContainer.jsx
+++ src/components/book/BookListByCourseContainer.jsx
@@ -1,7 +1,5 @@
+import React from "react";
+
 export function BookListByCourseContainer() {
-  return (
-    <>
-      <h1>BookListByCourseContainer funciona!</h1>
-    </>
-  );
+  return <h1>BookListByCourseContainer funciona!</h1>;
 }
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import HomePage from "../components/pages/HomePage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GenreListContainer from "../components/genre/GenreListContainer";
+import CourseListContainer from "../components/course/CourseListContainer";
+import BookDetailsContainer from "../components/book/BookDetailsContainer";
+import { BookListByCourseContainer } from "../components/book/BookListByCourseContainer";
+import BookAddForm from "../components/book/BookAddForm";
+import { NewBookAddForm } from "../components/book/NewBookAddForm";
+import GenreAddForm from "../components/genre/GenreAddForm";
+
+function Routes() {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/books/:id" component={BookDetailsContainer} />
+          <Route path="/genres" component={GenreListContainer} />
+          <Route path="/courses" component={CourseListContainer} />
+          <Route path="/addBook" component={BookAddForm} />
+          <Route path="/newAddBook" component={NewBookAddForm} />
+          <Route path="/addGenre" component={GenreAddForm} />
+          <Route
+            path="/bookListByCourse"
+            component={BookListByCourseContainer}
+          />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default Routes;
