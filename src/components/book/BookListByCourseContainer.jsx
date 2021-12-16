04a185df2807ce416a6af490600813766191dcbb
Mércio Andrade Filho <merciofilho@gmail.com>
2021-12-16 09:06:10 -0300

    add fazer login button

--- src/components/book/BookListByCourseContainer.jsx
+++ src/components/book/BookListByCourseContainer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// buscar livros por id
+// receber parâmetro (id) pela url
+
 export function BookListByCourseContainer() {
   return <h1>BookListByCourseContainer funciona!</h1>;
 }
